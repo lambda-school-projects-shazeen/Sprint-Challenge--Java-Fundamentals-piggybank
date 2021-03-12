f1de07aee966094f189a77e2bbb82ea389fbde0c
Shazeen Fabius <shazeenfabius@Shazeens-MBP.lan>
2021-03-12 14:51:51 -0800

    set up the controller methods for total

---
+++ src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
@@ -0,0 +1,35 @@
+package com.lambdaschool.piggybank.controllers;
+
+import com.lambdaschool.piggybank.models.Coin;
+import com.lambdaschool.piggybank.repositories.CoinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CoinController
+{
+    @Autowired
+    CoinRepository coinrepo;
+
+    //http://localhost:2019/total
+    @GetMapping(value = "/total", produces = {"application/json"})
+    public ResponseEntity<?> getCoinTotal()
+    {
+        List<Coin> myList = new ArrayList<>();
+        coinrepo.findAll().iterator().forEachRemaining(myList::add);
+
+        long total = 0;
+        for (Coin c : myList)
+        {
+            total = total + (int)(c.getValue() * c.getQuantity());
+        }
+        System.out.println("The piggy bank holds " + total);
+        return new ResponseEntity<>(myList, HttpStatus.OK);
+    }
+}
